df5cf5f2a3c8cdb19d1705ac3c30e8007f172eef
svsenthilvelsv <trydevdev@gmail.com>
2021-10-24 15:40:32 +0530

    Placement Commit

---
+++ Placedetails.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[100],personalmail[100],kongumail[100],home[100];
+    int rollno,phone,arrear,gpa1,gpa2,gpa3,cgpa;
+    printf("Enter your NAME         :");
+    scanf("%s",name);
+    printf("Enter your ROLLL NO     :");
+    scanf("%d",&rollno);
+     printf("Enter your PERSONAL MAIL :");
+    scanf("%d",personalmail);
+    printf("Enter your KONGUMAIL   :");
+    scanf("%s",kongumail);
+    printf("Enter your HOSTEL OR DS :");
+    scanf("%s",home);
+    printf("Enter your BLOOD GROUP  :");
+    scanf("%s",blood);
+    printf("Enter your NO OF ARREAR :");
+    scanf("%d",&arrear);
+    printf("Enter your GPA 1 :");
+    scanf("%d",&gpa1);
+    printf("Enter your GPA 2 :");
+    scanf("%d",&gpa2);
+    printf("Enter your GPA 3 :");
+    scanf("%d",&gpa3);
+     printf("Enter your CGPA :");
+    scanf("%d",&cgpa);
+    printf("Enter your PHONE NUMBER :");
+    printf("\n\n\t----DETAILS----\n");
+    printf("NAME          : %s",name);
+    printf("ROLL NO       : %d",rollno);
+    printf("E-MAIL personal : %s",personalmail);
+    printf("E-MAIL kongu : %s",kongumail);
+    printf("AADHAR        : %d",aadhar);
+    printf("PHONE         : %d",phone);
+    
+}
